788f33ce8ece44e6dfbe71045ae418fefffac90c
bodisB <bencebodis07@gmail.com>
2021-04-06 14:43:54 +0200

    database schema extended /zsombor

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,3 +1,13 @@
+ALTER TABLE IF EXISTS ONLY public.game_state DROP CONSTRAINT IF EXISTS fk_player_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.inventory DROP CONSTRAINT IF EXISTS fk_player_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.inventory DROP CONSTRAINT IF EXISTS fk_item_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.maps DROP CONSTRAINT IF EXISTS fk_game_state_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.maps DROP CONSTRAINT IF EXISTS fk_map_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.map_items DROP CONSTRAINT IF EXISTS fk_map_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.map_items DROP CONSTRAINT IF EXISTS fk_item_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.map_actors DROP CONSTRAINT IF EXISTS fk_map_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.map_actors DROP CONSTRAINT IF EXISTS fk_actor_id CASCADE;
+
 DROP TABLE IF EXISTS public.game_state;
 CREATE TABLE public.game_state
 (
@@ -47,5 +57,59 @@ CREATE TABLE public.map_actors
     y        integer NOT NULL
 );
 
+DROP TABLE IF EXISTS public.inventory;
+CREATE TABLE public.inventory
+(
+    player_id  integer NOT NULL,
+    item_id    integer NOT NULL,
+    item_count integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.actor;
+CREATE TABLE actor
+(
+    id   serial NOT NULL PRIMARY KEY,
+    name text   NOT NULL
+);
+
+DROP TABLE IF EXISTS public.map;
+CREATE TABLE public.map
+(
+    id             serial  NOT NULL PRIMARY KEY,
+    is_current_map boolean NOT NULL
+);
+
+DROP TABLE IF EXISTS public.item;
+CREATE TABLE item
+(
+    id   serial NOT NULL PRIMARY KEY,
+    name text   NOT NULL
+);
+
+
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+
+ALTER TABLE ONLY public.inventory
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+
+ALTER TABLE ONLY public.inventory
+    ADD CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES public.item(id);
+
+ALTER TABLE ONLY public.maps
+    ADD CONSTRAINT fk_game_state_id FOREIGN KEY (game_state_id) REFERENCES public.game_state(id);
+
+ALTER TABLE ONLY public.maps
+    ADD CONSTRAINT fk_map_id FOREIGN KEY (map_id) REFERENCES public.map(id);
+
+ALTER TABLE ONLY public.map_items
+    ADD CONSTRAINT fk_map_id FOREIGN KEY (map_id) REFERENCES public.map(id);
+
+ALTER TABLE ONLY public.map_items
+    ADD CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES public.item(id);
+
+ALTER TABLE ONLY public.map_actors
+    ADD CONSTRAINT fk_map_id FOREIGN KEY (map_id) REFERENCES public.map(id);
+
+ALTER TABLE ONLY public.map_actors
+    ADD CONSTRAINT fk_actor_id FOREIGN KEY (actor_id) REFERENCES public.actor(id);
